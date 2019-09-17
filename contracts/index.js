1500bb266f41bd3b8e73ced49ab8b435ffc5080e
Andy Gray <andy@whatsthescore.co.uk>
2019-09-17 11:12:25 +0100

    ropsten addresses

--- contracts/index.js
+++ contracts/index.js
@@ -15,6 +15,10 @@ module.exports = {
                 mainnet: {
                     address: '0x2f2d5aa0efdb9ca3c9bb789693d06bebea88792f',
                     abi: abi.BlockCities
+                },
+                ropsten: {
+                    address: '0x86eD0a82dDc2EdEA8cC4Bc023eC2a4079DAB42c9',
+                    abi: abi.BlockCities
                 }
             });
         },
@@ -22,6 +26,10 @@ module.exports = {
             return networkSplitter(network, {
                 mainnet: {
                     address: '0x24a896E29f79bE656F0216aE45E6cd79Bb80459b',
+                    abi: abi.BlockCitiesVendingMachine
+                },
+                ropsten: {
+                    address: '0xBEa2037e7bD516Ecd2f9fCAe8bFb1EF810b19204',
                     abi: abi.BlockCitiesVendingMachine
                 }
             });
@@ -31,6 +39,10 @@ module.exports = {
                 mainnet: {
                     address: '0x723f0128d909F059e4bEbBE9131ECfD02a85E4DE',
                     abi: abi.ColourGenerator
+                },
+                ropsten: {
+                    address: '0x14d1c1dce3c91d8a9c213f9f0a3f3c7b88388c10 ',
+                    abi: abi.ColourGenerator
                 }
             });
         },
@@ -38,6 +50,10 @@ module.exports = {
             return networkSplitter(network, {
                 mainnet: {
                     address: '0x94128bd30B2e8cfE106C24FEF181670deE4F20d8',
+                    abi: abi.LogicGenerator
+                },
+                ropsten: {
+                    address: '0x8e29c59713ac8d490429bea482ef5e3a320f39d1 ',
                     abi: abi.LogicGenerator
                 }
             });
