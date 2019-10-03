1288baabdaacfe1d58f4297b2f2181d0c8797a06
Andy Gray <andy@whatsthescore.co.uk>
2019-10-03 10:24:44 +0100

    new colour generator

--- contracts/index.js
+++ contracts/index.js
@@ -1,10 +1,10 @@
 const {networkSplitter} = require('../tools');
 
 const abi = {
-    BlockCities: require('./abis/v1/BlockCities'),
-    BlockCitiesVendingMachine: require('./abis/v2/BlockCitiesVendingMachineV2'),
-    ColourGenerator: require('./abis/v1/generators/ColourGenerator'),
-    LogicGenerator: require('./abis/v2/generators/LogicGeneratorV2'),
+    BlockCities: require('./abis/v1/BlockCities'), // v1
+    BlockCitiesVendingMachine: require('./abis/v2/BlockCitiesVendingMachineV2'), // v2
+    ColourGenerator: require('./abis/v2/generators/ColourGeneratorV2'), // v2
+    LogicGenerator: require('./abis/v3/generators/LogicGeneratorV2'), // v2 - have v3 ready
 };
 
 module.exports = {
@@ -19,7 +19,15 @@ module.exports = {
                 ropsten: {
                     address: '0x86eD0a82dDc2EdEA8cC4Bc023eC2a4079DAB42c9',
                     abi: abi.BlockCities
-                }
+                },
+                rinkeby: {
+                    address: '0x74b8D7E2b681d1C4f13bd8722937A722bCc7A4F3',
+                    abi: abi.BlockCities
+                },
+                local: {
+                    address: '0x70D0C5f857C0C57190566d45AaF53234b65B8bE9',
+                    abi: abi.BlockCities
+                },
             });
         },
         BlockCitiesVendingMachine: function (network) {
@@ -31,19 +39,35 @@ module.exports = {
                 ropsten: {
                     address: '0xBEa2037e7bD516Ecd2f9fCAe8bFb1EF810b19204',
                     abi: abi.BlockCitiesVendingMachine
-                }
+                },
+                rinkeby: {
+                    address: '0x018c994c2e15dA3a1aF1A9201E737688EA69F8aC',
+                    abi: abi.BlockCitiesVendingMachine
+                },
+                local: {
+                    address: '0xB5C37B3E64080081C446AA10C5f5aB20CBf55C04',
+                    abi: abi.BlockCitiesVendingMachine
+                },
             });
         },
         ColourGenerator: function (network) {
             return networkSplitter(network, {
                 mainnet: {
-                    address: '0x723f0128d909F059e4bEbBE9131ECfD02a85E4DE',
+                    address: '0x2fc79fa0f714d588835698ebe1965c511c03bb57',
                     abi: abi.ColourGenerator
                 },
                 ropsten: {
                     address: '0x14d1c1dce3c91d8a9c213f9f0a3f3c7b88388c10 ',
                     abi: abi.ColourGenerator
-                }
+                },
+                rinkeby: {
+                    address: '0x26DA251AfAd2625360C672f8e071Be73D48ca8A1',
+                    abi: abi.ColourGenerator
+                },
+                local: {
+                    address: '0x380A8Ee44B9127b9669897E21b923E562eDEd6aF',
+                    abi: abi.ColourGenerator
+                },
             });
         },
         LogicGenerator: function (network) {
@@ -55,7 +79,15 @@ module.exports = {
                 ropsten: {
                     address: '0x8e29c59713ac8d490429bea482ef5e3a320f39d1 ',
                     abi: abi.LogicGenerator
-                }
+                },
+                rinkeby: {
+                    address: '0x5329cFC4061cE82366464e6A06fcDc54927c0A11',
+                    abi: abi.LogicGenerator
+                },
+                local: {
+                    address: '0xFfE3da3717EBcCd1F52c138F0b07059F534AC8Fa',
+                    abi: abi.LogicGenerator
+                },
             });
         },
     }
